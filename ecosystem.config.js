1fe696f88815bc2e58228ed904a82e60e002fda9
Mr.Quoc Nguyen <quocnguyen753.pa@gmail.com>
2026-05-12 11:54:28 +0700

    feat: add ecosystem configuration for process management

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  apps: [
+    {
+      name: "portfolio-app",
+
+      // Lệnh chạy
+      script: "npm",
+
+      // Arguments cho script
+      args: "start",
+
+      // Chạy cluster để tận dụng nhiều CPU
+      exec_mode: "cluster",
+
+      // Số instance
+      // 1 = 1 process
+      // "max" = dùng toàn bộ CPU
+      instances: 1,
+
+      // Environment variables
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+
+      // Auto restart nếu crash
+      autorestart: true,
+
+      // Restart nếu dùng quá nhiều RAM
+      max_memory_restart: "1G",
+
+      // Delay trước khi restart
+      restart_delay: 5000
+    },
+  ],
+};
